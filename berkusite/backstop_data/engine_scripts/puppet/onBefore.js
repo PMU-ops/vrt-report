d6a4491a8beff781585ca248b98fa605b9fb2196
Emelio Steve Malisa <managedupdates@pantheon.io>
2025-10-08 08:37:59 +0800

    berkusite/mu-100825.2

--- berkusite/backstop_data/engine_scripts/puppet/onBefore.js
+++ berkusite/backstop_data/engine_scripts/puppet/onBefore.js
@@ -1,3 +1,11 @@
 module.exports = async (page, scenario, vp) => {
   await require('./loadCookies')(page, scenario);
+
+  const cacheBuster = `cb=${Date.now()}${Math.floor(Math.random() * 1000)}`;
+    if (scenario.url) {
+      scenario.url += (scenario.url.includes('?') ? '&' : '?') + cacheBuster;
+    }
+    if (scenario.referenceUrl) {
+      scenario.referenceUrl += (scenario.referenceUrl.includes('?') ? '&' : '?') + cacheBuster;
+    }
 };
