095f31698556b866c605263d67846a10c792f254
Emelio Steve Malisa <managedupdates@pantheon.io>
2025-10-30 14:59:13 +0800

    vertafore/mu-103025

---
+++ vertafore/backstop_data/html_report/config.js
@@ -0,0 +1,221 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Home_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20251030-135153/backstop_default_Home_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Home_0_document_0_desktop.png",
+        "label": "Home",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.vertafore.com/",
+        "referenceUrl": "https://autopilot-vertafore.pantheonsite.io/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -342
+          },
+          "rawMisMatchPercentage": 12.70322741521801,
+          "misMatchPercentage": "12.70",
+          "analysisTime": 247
+        },
+        "diffImage": "../bitmaps_test/20251030-135153/failed_diff_backstop_default_Home_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Products_-_Commercial_Submissions_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20251030-135153/backstop_default_Products_-_Commercial_Submissions_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Products_-_Commercial_Submissions_0_document_0_desktop.png",
+        "label": "Products - Commercial Submissions",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.vertafore.com/products/commercial-submissions",
+        "referenceUrl": "https://autopilot-vertafore.pantheonsite.io/products/commercial-submissions",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 2.4601119590741365,
+          "misMatchPercentage": "2.46",
+          "analysisTime": 497
+        },
+        "diffImage": "../bitmaps_test/20251030-135153/failed_diff_backstop_default_Products_-_Commercial_Submissions_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Products_-_PL_Rating_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20251030-135153/backstop_default_Products_-_PL_Rating_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Products_-_PL_Rating_0_document_0_desktop.png",
+        "label": "Products - PL Rating",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.vertafore.com/products/pl-rating",
+        "referenceUrl": "https://autopilot-vertafore.pantheonsite.io/products/pl-rating",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 2.566913901585689,
+          "misMatchPercentage": "2.57",
+          "analysisTime": 512
+        },
+        "diffImage": "../bitmaps_test/20251030-135153/failed_diff_backstop_default_Products_-_PL_Rating_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_About_-_Our_Story_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20251030-135153/backstop_default_About_-_Our_Story_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_About_-_Our_Story_0_document_0_desktop.png",
+        "label": "About - Our Story",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.vertafore.com/about/our-story",
+        "referenceUrl": "https://autopilot-vertafore.pantheonsite.io/about/our-story",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.8642404343623857,
+          "misMatchPercentage": "3.86",
+          "analysisTime": 208
+        },
+        "diffImage": "../bitmaps_test/20251030-135153/failed_diff_backstop_default_About_-_Our_Story_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Resources_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20251030-135153/backstop_default_Resources_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Resources_0_document_0_desktop.png",
+        "label": "Resources",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.vertafore.com/resources",
+        "referenceUrl": "https://autopilot-vertafore.pantheonsite.io/resources",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 8.494194770526594,
+          "misMatchPercentage": "8.49",
+          "analysisTime": 388
+        },
+        "diffImage": "../bitmaps_test/20251030-135153/failed_diff_backstop_default_Resources_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_About_-_Blog_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20251030-135153/backstop_default_About_-_Blog_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_About_-_Blog_0_document_0_desktop.png",
+        "label": "About - Blog",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.vertafore.com/about/blog",
+        "referenceUrl": "https://autopilot-vertafore.pantheonsite.io/about/blog",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -55
+          },
+          "rawMisMatchPercentage": 12.765925674803047,
+          "misMatchPercentage": "12.77",
+          "analysisTime": 433
+        },
+        "diffImage": "../bitmaps_test/20251030-135153/failed_diff_backstop_default_About_-_Blog_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Resources_Blog_-_Future_Commercial_Insurance_Depends_on_Connectivity_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20251030-135153/backstop_default_Resources_Blog_-_Future_Commercial_Insurance_Depends_on_Connectivity_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Resources_Blog_-_Future_Commercial_Insurance_Depends_on_Connectivity_0_document_0_desktop.png",
+        "label": "Resources Blog - Future Commercial Insurance Depends on Connectivity",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.vertafore.com/resources/blog/future-commercial-insurance-depends-connectivity",
+        "referenceUrl": "https://autopilot-vertafore.pantheonsite.io/resources/blog/future-commercial-insurance-depends-connectivity",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.07955749437813527,
+          "misMatchPercentage": "0.08",
+          "analysisTime": 355
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_About_-_Events_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20251030-135153/backstop_default_About_-_Events_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_About_-_Events_0_document_0_desktop.png",
+        "label": "About - Events",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.vertafore.com/about/events",
+        "referenceUrl": "https://autopilot-vertafore.pantheonsite.io/about/events",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -2127
+          },
+          "rawMisMatchPercentage": 21.94504385658373,
+          "misMatchPercentage": "21.95",
+          "analysisTime": 497
+        },
+        "diffImage": "../bitmaps_test/20251030-135153/failed_diff_backstop_default_About_-_Events_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "backstop_default"
+});
