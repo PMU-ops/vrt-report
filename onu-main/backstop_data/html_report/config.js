d192c892a6fdc67cd6177ba1f47ae47e6439f6cc
Emelio Steve Malisa <managedupdates@pantheon.io>
2026-01-12 17:35:57 +0800

    onu-main/mu-011226

---
+++ onu-main/backstop_data/html_report/config.js
@@ -0,0 +1,360 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Home_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Home_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Home_0_document_0_desktop.png",
+        "label": "Home",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 3.7012687836688403,
+          "misMatchPercentage": "3.70",
+          "analysisTime": 214
+        },
+        "diffImage": "../bitmaps_test/20260112-173340/failed_diff_backstop_default_Home_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Apply_to_ONU_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Apply_to_ONU_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Apply_to_ONU_0_document_0_desktop.png",
+        "label": "Apply to ONU",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/admissions-aid/apply-onu",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/admissions-aid/apply-onu",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.00000732124449442414,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 225
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Visit_ONU_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Visit_ONU_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Visit_ONU_0_document_0_desktop.png",
+        "label": "Visit ONU",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/admissions-aid/visit",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/admissions-aid/visit",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.0035046728971962616,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 390
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Academics_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Academics_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Academics_0_document_0_desktop.png",
+        "label": "Academics",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/academics",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/academics",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_News__Newman_Civic_Fellow_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_News__Newman_Civic_Fellow_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_News__Newman_Civic_Fellow_0_document_0_desktop.png",
+        "label": "News – Newman Civic Fellow",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/news/onu-pharmacy-major-and-lgbtq-activist-joshua-honaker-named-newman-civic-fellow",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/news/onu-pharmacy-major-and-lgbtq-activist-joshua-honaker-named-newman-civic-fellow",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_News__Commencement_Spotlights_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_News__Commencement_Spotlights_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_News__Commencement_Spotlights_0_document_0_desktop.png",
+        "label": "News – Commencement Spotlights",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/news/onu-commencement-23-student-spotlights",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/news/onu-commencement-23-student-spotlights",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Bioengineering_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Bioengineering_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Bioengineering_0_document_0_desktop.png",
+        "label": "Bioengineering",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/academics/bioengineering",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/academics/bioengineering",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 237
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Financial_Aid_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Financial_Aid_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Financial_Aid_0_document_0_desktop.png",
+        "label": "Financial Aid",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/financial-aid",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/financial-aid",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Undergraduate_Scholarships_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Undergraduate_Scholarships_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Undergraduate_Scholarships_0_document_0_desktop.png",
+        "label": "Undergraduate Scholarships",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/admissions-aid/financial-aid/undergraduate-scholarships",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/admissions-aid/financial-aid/undergraduate-scholarships",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Town__Gown_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Town__Gown_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Town__Gown_0_document_0_desktop.png",
+        "label": "Town & Gown",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/towngown",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/towngown",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Admissions__Aid_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Admissions__Aid_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Admissions__Aid_0_document_0_desktop.png",
+        "label": "Admissions & Aid",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/admissions-aid",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/admissions-aid",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 50
+          },
+          "rawMisMatchPercentage": 0.0006608080204990422,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 531
+        },
+        "diffImage": "../bitmaps_test/20260112-173340/failed_diff_backstop_default_Admissions__Aid_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Homecoming_Event_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_Homecoming_Event_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Homecoming_Event_0_document_0_desktop.png",
+        "label": "Homecoming Event",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/event/homecoming",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/event/homecoming",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 79
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_ONU_at_a_Glance_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_ONU_at_a_Glance_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_ONU_at_a_Glance_0_document_0_desktop.png",
+        "label": "ONU at a Glance",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/about/onu-glance",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/about/onu-glance",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 193
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_FREED_Center_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260112-173340/backstop_default_FREED_Center_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_FREED_Center_0_document_0_desktop.png",
+        "label": "FREED Center",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.onu.edu/freed",
+        "referenceUrl": "https://mu-011226-onu-main-drupal-9.pantheonsite.io/freed",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.000007040191042624133,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 229
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
