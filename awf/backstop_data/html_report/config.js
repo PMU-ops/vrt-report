9d94b6a85966b44cb12e4152efa3e84bc6ef94d6
Emelio Steve Malisa <managedupdates@pantheon.io>
2026-01-29 13:51:59 +0800

    awf/mu-012926

--- awf/backstop_data/html_report/config.js
+++ awf/backstop_data/html_report/config.js
@@ -4,41 +4,41 @@ report({
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Home_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20260113-112647/backstop_default_Home_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260129-134324/backstop_default_Home_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_Home_0_document_0_desktop.png",
         "label": "Home",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "https://www.awf.org/",
-        "referenceUrl": "https://mu-011326-awfd8-sandbox.pantheonsite.io/",
+        "referenceUrl": "https://mu-012926-awfd8-sandbox.pantheonsite.io/",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
           "isSameDimensions": false,
           "dimensionDifference": {
             "width": 0,
-            "height": 23
+            "height": -29
           },
-          "rawMisMatchPercentage": 25.61774096068951,
-          "misMatchPercentage": "25.62",
-          "analysisTime": 442
+          "rawMisMatchPercentage": 17.44845717448457,
+          "misMatchPercentage": "17.45",
+          "analysisTime": 283
         },
-        "diffImage": "../bitmaps_test/20260113-112647/failed_diff_backstop_default_Home_0_document_0_desktop.png"
+        "diffImage": "../bitmaps_test/20260129-134324/failed_diff_backstop_default_Home_0_document_0_desktop.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Wildlife_Conservation__All_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20260113-112647/backstop_default_Wildlife_Conservation__All_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260129-134324/backstop_default_Wildlife_Conservation__All_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_Wildlife_Conservation__All_0_document_0_desktop.png",
         "label": "Wildlife Conservation – All",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "https://www.awf.org/wildlife-conservation/all",
-        "referenceUrl": "https://mu-011326-awfd8-sandbox.pantheonsite.io/wildlife-conservation/all",
+        "referenceUrl": "https://mu-012926-awfd8-sandbox.pantheonsite.io/wildlife-conservation/all",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
@@ -47,25 +47,24 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 8.131341185652396,
-          "misMatchPercentage": "8.13",
-          "analysisTime": 407
-        },
-        "diffImage": "../bitmaps_test/20260113-112647/failed_diff_backstop_default_Wildlife_Conservation__All_0_document_0_desktop.png"
+          "rawMisMatchPercentage": 0.006968335881753313,
+          "misMatchPercentage": "0.01",
+          "analysisTime": 248
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Wildlife_Conservation__Pangolin_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20260113-112647/backstop_default_Wildlife_Conservation__Pangolin_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260129-134324/backstop_default_Wildlife_Conservation__Pangolin_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_Wildlife_Conservation__Pangolin_0_document_0_desktop.png",
         "label": "Wildlife Conservation – Pangolin",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "https://www.awf.org/wildlife-conservation/pangolin",
-        "referenceUrl": "https://mu-011326-awfd8-sandbox.pantheonsite.io/wildlife-conservation/pangolin",
+        "referenceUrl": "https://mu-012926-awfd8-sandbox.pantheonsite.io/wildlife-conservation/pangolin",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
@@ -74,25 +73,24 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 3.936460225919907,
-          "misMatchPercentage": "3.94",
-          "analysisTime": 364
-        },
-        "diffImage": "../bitmaps_test/20260113-112647/failed_diff_backstop_default_Wildlife_Conservation__Pangolin_0_document_0_desktop.png"
+          "rawMisMatchPercentage": 0.003977174201820909,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 175
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_News_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20260113-112647/backstop_default_News_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260129-134324/backstop_default_News_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_News_0_document_0_desktop.png",
         "label": "News",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "https://www.awf.org/news",
-        "referenceUrl": "https://mu-011326-awfd8-sandbox.pantheonsite.io/news",
+        "referenceUrl": "https://mu-012926-awfd8-sandbox.pantheonsite.io/news",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
@@ -101,25 +99,24 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 1.9494398199047172,
-          "misMatchPercentage": "1.95",
-          "analysisTime": 559
-        },
-        "diffImage": "../bitmaps_test/20260113-112647/failed_diff_backstop_default_News_0_document_0_desktop.png"
+          "rawMisMatchPercentage": 0.02032242528376549,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 326
+        }
       },
-      "status": "fail"
+      "status": "pass"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_Safaris_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20260113-112647/backstop_default_Safaris_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260129-134324/backstop_default_Safaris_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_Safaris_0_document_0_desktop.png",
         "label": "Safaris",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "https://www.awf.org/safaris",
-        "referenceUrl": "https://mu-011326-awfd8-sandbox.pantheonsite.io/safaris",
+        "referenceUrl": "https://mu-012926-awfd8-sandbox.pantheonsite.io/safaris",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
@@ -128,25 +125,25 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 18.457459620909535,
-          "misMatchPercentage": "18.46",
-          "analysisTime": 554
+          "rawMisMatchPercentage": 16.451933720769336,
+          "misMatchPercentage": "16.45",
+          "analysisTime": 260
         },
-        "diffImage": "../bitmaps_test/20260113-112647/failed_diff_backstop_default_Safaris_0_document_0_desktop.png"
+        "diffImage": "../bitmaps_test/20260129-134324/failed_diff_backstop_default_Safaris_0_document_0_desktop.png"
       },
       "status": "fail"
     },
     {
       "pair": {
         "reference": "../bitmaps_reference/backstop_default_News__Mystery_Giraffes_0_document_0_desktop.png",
-        "test": "../bitmaps_test/20260113-112647/backstop_default_News__Mystery_Giraffes_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260129-134324/backstop_default_News__Mystery_Giraffes_0_document_0_desktop.png",
         "selector": "document",
         "fileName": "backstop_default_News__Mystery_Giraffes_0_document_0_desktop.png",
         "label": "News – Mystery Giraffes",
         "requireSameDimensions": true,
         "misMatchThreshold": 0.1,
         "url": "https://www.awf.org/news/mystery-giraffes-africa",
-        "referenceUrl": "https://mu-011326-awfd8-sandbox.pantheonsite.io/news/mystery-giraffes-africa",
+        "referenceUrl": "https://mu-012926-awfd8-sandbox.pantheonsite.io/news/mystery-giraffes-africa",
         "expect": 0,
         "viewportLabel": "desktop",
         "diff": {
@@ -155,12 +152,13 @@ report({
             "width": 0,
             "height": 0
           },
-          "rawMisMatchPercentage": 0.006800659081017223,
-          "misMatchPercentage": "0.01",
-          "analysisTime": 260
-        }
+          "rawMisMatchPercentage": 6.3090880121867805,
+          "misMatchPercentage": "6.31",
+          "analysisTime": 219
+        },
+        "diffImage": "../bitmaps_test/20260129-134324/failed_diff_backstop_default_News__Mystery_Giraffes_0_document_0_desktop.png"
       },
-      "status": "pass"
+      "status": "fail"
     }
   ],
   "id": "backstop_default"
