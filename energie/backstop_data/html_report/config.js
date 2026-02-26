88e13871dbffb828c10843c27105b741c3a1eb0b
Emelio Steve Malisa <managedupdates@pantheon.io>
2026-02-26 10:27:50 +0800

    energie/mu-022626

---
+++ energie/backstop_data/html_report/config.js
@@ -0,0 +1,138 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Homepage_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260226-101302/backstop_default_Homepage_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Homepage_0_document_0_desktop.png",
+        "label": "Homepage",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://energielalievre.com/?cb=1772071983725873",
+        "referenceUrl": "https://autopilot-energie-la-lievre.pantheonsite.io/?cb=1772071983725873",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.21326128330614466,
+          "misMatchPercentage": "0.21",
+          "analysisTime": 232
+        },
+        "diffImage": "../bitmaps_test/20260226-101302/failed_diff_backstop_default_Homepage_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Gestion_de_leau_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260226-101302/backstop_default_Gestion_de_leau_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Gestion_de_leau_0_document_0_desktop.png",
+        "label": "Gestion de l'eau",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://energielalievre.com/gestion-de-leau/?cb=1772071983737570",
+        "referenceUrl": "https://autopilot-energie-la-lievre.pantheonsite.io/gestion-de-leau/?cb=1772071983737570",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": false,
+          "dimensionDifference": {
+            "width": 0,
+            "height": -16
+          },
+          "rawMisMatchPercentage": 5.148127209399044,
+          "misMatchPercentage": "5.15",
+          "analysisTime": 189
+        },
+        "diffImage": "../bitmaps_test/20260226-101302/failed_diff_backstop_default_Gestion_de_leau_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Scurit_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260226-101302/backstop_default_Scurit_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Scurit_0_document_0_desktop.png",
+        "label": "Sécurité",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://energielalievre.com/securite/?cb=177207198353399",
+        "referenceUrl": "https://autopilot-energie-la-lievre.pantheonsite.io/securite/?cb=177207198353399",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.015144494783086217,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 143
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Communaut_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260226-101302/backstop_default_Communaut_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Communaut_0_document_0_desktop.png",
+        "label": "Communauté",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://energielalievre.com/communaute/?cb=1772071983784741",
+        "referenceUrl": "https://autopilot-energie-la-lievre.pantheonsite.io/communaute/?cb=1772071983784741",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.008788733580330077,
+          "misMatchPercentage": "0.01",
+          "analysisTime": 161
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Contact_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260226-101302/backstop_default_Contact_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Contact_0_document_0_desktop.png",
+        "label": "Contact",
+        "requireSameDimensions": false,
+        "misMatchThreshold": 0.1,
+        "url": "https://energielalievre.com/contact/?cb=1772071983802440",
+        "referenceUrl": "https://autopilot-energie-la-lievre.pantheonsite.io/contact/?cb=1772071983802440",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.07070022101667671,
+          "misMatchPercentage": "0.07",
+          "analysisTime": 109
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
