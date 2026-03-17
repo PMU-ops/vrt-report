27ef80e8c5b236ace71434e0fd188d0608c55aa8
Steve Malisa <steve.malisa@GG4NJ76RPQ-steve.malisa>
2026-03-17 13:53:04 +0800

    coastal/mu-031726

---
+++ coastal/backstop_data/html_report/config.js
@@ -0,0 +1,263 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Home_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Home_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Home_0_document_0_desktop.png",
+        "label": "Home",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 74
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Ship_Western_Alaska_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Ship_Western_Alaska_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Ship_Western_Alaska_0_document_0_desktop.png",
+        "label": "Ship Western Alaska",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/ship-western-alaska",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/ship-western-alaska",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.0031676599429502853,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 101
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Seattle_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Seattle_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Seattle_0_document_0_desktop.png",
+        "label": "Seattle",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/seattle",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/seattle",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.005835914039039039,
+          "misMatchPercentage": "0.01",
+          "analysisTime": 79
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Services_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Services_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Services_0_document_0_desktop.png",
+        "label": "Services",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/services",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/services",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.003224823688031529,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 86
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Sailing_Schedule_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Sailing_Schedule_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Sailing_Schedule_0_document_0_desktop.png",
+        "label": "Sailing Schedule",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/sailing-schedule",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/sailing-schedule",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.0028694859726836473,
+          "misMatchPercentage": "0.00",
+          "analysisTime": 110
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Booking_Request_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Booking_Request_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Booking_Request_0_document_0_desktop.png",
+        "label": "Booking Request",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/booking-request",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/booking-request",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Track_the_Fleet_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Track_the_Fleet_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Track_the_Fleet_0_document_0_desktop.png",
+        "label": "Track the Fleet",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/track-the-fleet",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/track-the-fleet",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 15.653193023865722,
+          "misMatchPercentage": "15.65",
+          "analysisTime": 42
+        },
+        "diffImage": "../bitmaps_test/20260317-134742/failed_diff_backstop_default_Track_the_Fleet_0_document_0_desktop.png"
+      },
+      "status": "fail"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Freight_Tariff_Customer_Forms_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Freight_Tariff_Customer_Forms_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Freight_Tariff_Customer_Forms_0_document_0_desktop.png",
+        "label": "Freight Tariff Customer Forms",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/freight-tariff-customer-forms",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/freight-tariff-customer-forms",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Dutch_Harbor_Alaska_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Dutch_Harbor_Alaska_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Dutch_Harbor_Alaska_0_document_0_desktop.png",
+        "label": "Dutch Harbor Alaska",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/dutch-harbor-alaska",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/dutch-harbor-alaska",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.0060896494320407365,
+          "misMatchPercentage": "0.01",
+          "analysisTime": 74
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/backstop_default_Construction_Heavy_Equipment_0_document_0_desktop.png",
+        "test": "../bitmaps_test/20260317-134742/backstop_default_Construction_Heavy_Equipment_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "backstop_default_Construction_Heavy_Equipment_0_document_0_desktop.png",
+        "label": "Construction Heavy Equipment",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://www.coastaltransportation.com/construction-heavy-equipment",
+        "referenceUrl": "https://mu-031726-coastal-transportation.pantheonsite.io/construction-heavy-equipment",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.010639953448089207,
+          "misMatchPercentage": "0.01",
+          "analysisTime": 103
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "backstop_default"
+});
